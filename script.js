8ce8b1ea608455f34528cc8a6183c832a159bf18
Sara D'Arce <saradarce@gmail.com>
2023-11-28 18:26:14 -0500

    Fizz Buzz

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Practical Loops!");
+
+//Fizz Buzz
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 === 0) {
+    console.log("FIZZ BUZZ");
+  } else if (i % 3 === 0) {
+    console.log("FIZZ");
+  } else if (i % 5 === 0) {
+    console.log("BUZZ");
+  } else { console.log(i);
+}
