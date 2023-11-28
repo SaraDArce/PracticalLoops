62d3d5012104a7eb14607ba256a065ecd7bdbd07
Sara D'Arce <saradarce@gmail.com>
2023-11-28 18:33:48 -0500

    Prime Time

--- script.js
+++ script.js
@@ -11,3 +11,40 @@ for (let i = 1; i <= 100; i++) {
     console.log("BUZZ");
   } else { console.log(i);
 }
+
+//2 Prime Time
+
+for (let i = 2; i < 100; i++) {
+    let isPrime = true;
+    console.log("outer: ", i);
+    for (let j = 2; j < i; j++) {
+      console.log("inner: ", j);
+      if (i % j === 0) {
+        // if this is true, it is NOT a prime number
+        isPrime = false;
+        break;
+      }
+    }
+  
+    if (isPrime === true) {
+      console.log(i, "is a prime number");
+    }
+  }
+  
+
+  for (let i = 1; i < 200; i++) {
+    let isPrime = true;
+    let n = 84;
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        // if this is true, it is NOT a prime number
+        isPrime = false;
+        break;
+      }
+    }
+  }
+  
+    if (isPrime === true && i > n) {
+      console.log(i);
+      break;
+    }
